077d0e0b2f1c7814521307d30fced1439377a2b1
cankat1980@gmail.com <cankat1980@gmail.com>
2026-02-18 22:02:50 +0000

    2nd

--- schema.sql
+++ schema.sql
@@ -1,7 +1,4 @@
 # schema.yml
-sources:
-- name: gz_raw_data
-tables:
-- name: raw_gz_sales
-- name: raw_gz_product
-- name: raw_gz_ship
+{{sources(`gz_raw_data`,`raw_gz_sales`,`raw_gz_product`,`raw_gz_ship)}}
+
+
